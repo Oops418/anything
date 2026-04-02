c037e4b0ec6667b4a6ffd16aad6934bf4591ee62
kxyang <kx.yang.apply@outlook.com>
2026-04-02 19:57:48 +0800

    switch to native macos ui app

---
+++ crates/ui/macos/anything/anythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  anythingApp.swift
+//  anything
+//
+//  Created by kxyang on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct anythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
